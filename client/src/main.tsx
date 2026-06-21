92872f6637a557679b2dbed77bf99c5397f1375a
maxymuusha-hash <maxymuusha@gmail.com>
2026-06-21 14:37:19 +0200

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { trpc } from "./lib/trpc";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const queryClient = new QueryClient();
+
+const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: import.meta.env.VITE_SERVER_URL
+        ? `${import.meta.env.VITE_SERVER_URL}/trpc`
+        : "/trpc",
+    }),
+  ],
+});
+
+createRoot(document.getElementById("root")!).render(
+  <trpc.Provider client={trpcClient} queryClient={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </trpc.Provider>
+);
