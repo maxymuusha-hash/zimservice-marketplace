f62639159a4786e1071b275beaa372152e2863ec
maxymuusha-hash <maxymuusha@gmail.com>
2026-06-21 16:09:51 +0200

    Update trpc.ts

--- server/_core/trpc.ts
+++ server/_core/trpc.ts
@@ -1,22 +1,17 @@
 import { NOT_ADMIN_ERR_MSG, UNAUTHED_ERR_MSG } from '@shared/const';
 import { initTRPC, TRPCError } from "@trpc/server";
-import superjson from "superjson";
 import type { TrpcContext } from "./context";
 
-const t = initTRPC.context<TrpcContext>().create({
-  transformer: superjson,
-});
+const t = initTRPC.context<TrpcContext>().create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
 
 const requireUser = t.middleware(async opts => {
   const { ctx, next } = opts;
-
   if (!ctx.user) {
     throw new TRPCError({ code: "UNAUTHORIZED", message: UNAUTHED_ERR_MSG });
   }
-
   return next({
     ctx: {
       ...ctx,
@@ -30,11 +25,9 @@ export const protectedProcedure = t.procedure.use(requireUser);
 export const adminProcedure = t.procedure.use(
   t.middleware(async opts => {
     const { ctx, next } = opts;
-
     if (!ctx.user || ctx.user.role !== 'admin') {
       throw new TRPCError({ code: "FORBIDDEN", message: NOT_ADMIN_ERR_MSG });
     }
-
     return next({
       ctx: {
         ...ctx,
